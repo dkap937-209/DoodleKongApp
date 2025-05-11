6ab788369029bdb6b8b684b8bf224325fdbfdbac
Darpan <darpan.kapadia@gmail.com>
2025-05-11 16:55:57 +1200

    Dagger hilt setup

---
+++ app/src/main/java/com/dk/doodlekong/util/DispatcherProvider.kt
@@ -0,0 +1,9 @@
+package com.dk.doodlekong.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+}
