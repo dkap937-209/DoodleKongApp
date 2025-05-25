a007b785ee0aa337a145437617169aba301015a7
Darpan <darpan.kapadia@gmail.com>
2025-05-25 19:36:13 +1200

    Player navigation drawer

--- app/src/main/java/com/dk/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/dk/doodlekong/ui/drawing/DrawingActivity.kt
@@ -3,8 +3,16 @@ package com.dk.doodlekong.ui.drawing
 import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import androidx.recyclerview.widget.RecyclerView
 import com.dk.doodlekong.R
 import com.dk.doodlekong.databinding.ActivityDrawingBinding
 import com.dk.doodlekong.util.Constants
@@ -20,11 +28,38 @@ class DrawingActivity: AppCompatActivity() {
 
     private val viewModel by viewModels<DrawingViewModel>()
 
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var rvPlayers: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         subscribeToUiStateUpdates()
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        toggle.syncState()
+
+        val header = layoutInflater.inflate(R.layout.nav_drawer_header, binding.navView)
+        rvPlayers = header.findViewById(R.id.rvPlayers)
+        binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+        binding.ibPlayers.setOnClickListener{
+            binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            binding.root.openDrawer(GravityCompat.START)
+        }
+
+        binding.root.addDrawerListener(object: DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) = Unit
+
+            override fun onDrawerOpened(drawerView: View)  = Unit
+
+            override fun onDrawerClosed(drawerView: View) {
+                binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+
+            override fun onDrawerStateChanged(newState: Int) = Unit
+        })
 
         binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
             viewModel.checkRadioButton(checkedId)
@@ -54,5 +89,12 @@ class DrawingActivity: AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
